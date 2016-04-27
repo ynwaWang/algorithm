9e5a5ffb4f753ad3ee1f202a359e4b7379a113cf
wangdaowei <ynwa_wang@foxmail.com>
2016-04-27 16:38:23 +0800

    first commit

---
+++ src/main/java/Welcome.java
@@ -0,0 +1,8 @@
+/**
+ * Created by David Wang on 2016/4/27.
+ */
+public class Welcome {
+    static {
+        System.out.println("Welcome !!");
+    }
+}
---
+++ src/test/java/TestMain.java
@@ -0,0 +1,11 @@
+import org.junit.Test;
+
+/**
+ * Created by David Wang on 2016/4/27.
+ */
+public class TestMain {
+    @Test
+    public void welcome() {
+        new Welcome();
+    }
+}
