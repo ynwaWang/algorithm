b5622a371e9b4d0270f51646120e6d466bc9bc6a
ynwa <ynwa_wang@foxmail.com>
2016-07-05 23:51:22 +0800

    Class.forName performance problem

--- src/main/java/ynwa/fuji/Welcome.java
+++ src/main/java/ynwa/fuji/Welcome.java
@@ -9,7 +9,21 @@ public class Welcome {
 
     private String hi;
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IllegalAccessException, InstantiationException, ClassNotFoundException {
+        Class[] c = new Class[] {Welcome.class};
+        String name = "ynwa.fuji.Welcome";
+        long start1 = System.currentTimeMillis();
+        for (int i=0; i<1000000; i++) {
+            Welcome w1 = (Welcome) c[0].newInstance();
+        }
+        System.err.println(System.currentTimeMillis() - start1);
+        long start2 = System.currentTimeMillis();
+        for (int i=0; i<1000000; i++) {
+            Welcome w2 = (Welcome) Class.forName(name).newInstance();
+        }
+        System.err.println(System.currentTimeMillis() - start2);
+
+
         System.out.println("ynwa.fuji.Welcome !!");
         System.err.println("ssss");
     }
