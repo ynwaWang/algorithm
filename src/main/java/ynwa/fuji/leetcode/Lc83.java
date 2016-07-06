8126fb1deda37096426c45d495c384ef5c3cd082
wangdaowei <ynwa_wang@foxmail.com>
2016-07-06 16:12:50 +0800

    Lc83+git ignore

---
+++ src/main/java/ynwa/fuji/leetcode/Lc83.java
@@ -0,0 +1,46 @@
+package ynwa.fuji.leetcode;
+
+/**
+ * Created by David Wang<wdw@winbaoxian.com> on 2016-07-06.
+ * https://leetcode.com/problems/remove-duplicates-from-sorted-list/
+ */
+public class Lc83 {
+
+    public static class ListNode {
+        int val;
+        ListNode next;
+
+        ListNode(int x) {
+            val = x;
+        }
+    }
+
+    public static ListNode deleteDuplicates(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode cursor = head;
+        int key = cursor.val;
+        while (cursor.next != null) {
+            if (cursor.next.val != key) {
+                cursor = cursor.next;
+                key = cursor.val;
+            } else {
+                cursor.next = cursor.next.next;
+            }
+        }
+        return head;
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1,2,2};
+        ListNode head = new ListNode(a[0]);
+        ListNode cursor = head;
+        for (int i = 1; i < a.length; i++) {
+            cursor.next = new ListNode(a[i]);
+            cursor = cursor.next;
+        }
+
+        deleteDuplicates(head);
+    }
+}
---
+++ src/test/java/ynwa/fuji/BSTTest.java
@@ -0,0 +1,26 @@
+package ynwa.fuji;
+
+import edu.princeton.cs.algs4.BST;
+import edu.princeton.cs.algs4.StdOut;
+import org.junit.Test;
+
+/**
+ * Created by David Wang<wdw@winbaoxian.com> on 2016-07-05.
+ */
+public class BSTTest {
+    @Test
+    public void t1() {
+        BST<String, Integer> st = new BST<String, Integer>();
+//        String[] example = "S E R C H X A M P L D".split(" ");
+        String[] example = "E R C S H X M P L D".split(" ");
+        for (int i = 0; i < example.length; i++) {
+            String key = example[i];
+            st.put(key, i);
+        }
+//        st.height();
+//        for (String s : st.levelOrder())
+//            StdOut.println(s + " " + st.get(s));
+
+        st.deleteMin();
+    }
+}
