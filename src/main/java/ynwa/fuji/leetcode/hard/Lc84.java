d2ef447e20a12d775767e4ce8236c7f267e5c5d5
ynwa <ynwa_wang@foxmail.com>
2016-07-08 09:15:27 +0800

    add Lc84 largest rectangle in histogram

---
+++ src/main/java/ynwa/fuji/leetcode/hard/Lc84.java
@@ -0,0 +1,9 @@
+package ynwa.fuji.leetcode.hard;
+
+/**
+ * Created by ynwa on 16/7/8.
+ * https://leetcode.com/problems/largest-rectangle-in-histogram/
+ * TODO 基本思路:最大矩形最有可能出现在最大值周边,即左边/右边/两边(和最大值是相连的,也就是取最长矩形,其最低*长度),这样就实现了子最优
+ */
+public class Lc84 {
+}
